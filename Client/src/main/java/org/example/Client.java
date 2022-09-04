47c81eea32cd0cf76fe26791eb110b03870fda79
Step7439 <zamashikovs@mail.ru>
2022-09-04 22:29:48 +0900

    Добавил методы

--- Client/src/main/java/org/example/Client.java
+++ Client/src/main/java/org/example/Client.java
@@ -2,65 +2,115 @@ package org.example;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Client {
-    PrintWriter out;
-    BufferedReader in;
-    String resp;
-    Socket clientSocket;
+    private BufferedWriter out;
+    private BufferedReader in;
 
-    public static void main(String[] args) throws IOException {
-        Client client = new Client();
-        client.clients();
+    private Socket clientSocket;
+    private BufferedReader user;
+    private String userNick;
 
-    }
 
-    public void clients() throws IOException {
-        String url = "/home/acer/IdeaProjects/ServerChat/settings.txt";
+    public Client() throws IOException {
+        String url = "/home/acer/IdeaProjects/ServerChat/settings.txt";  // Запуск клиента через файл
         File setings = new File(url);
         Scanner scanner1 = new Scanner(setings);
         String hostPort = scanner1.nextLine();
         String[] hostPortMas = hostPort.split(":");
         String host = hostPortMas[0];
         int port = Integer.parseInt(hostPortMas[1]);
+        try {
+            clientSocket = new Socket(host, port);            // Подключения к серверу
+        } catch (IOException e) {
+            System.out.println("Ошибка сокет не создан!");
+        }
 
-
-        clientSocket = new Socket(host, port);
-
-        scanner1.close();
-        new Thread(() -> {
-
-            try {
-                while (true) {
-                    Scanner scanner = new Scanner(System.in);
-                    System.out.print("- > ");
-                    String name = scanner.nextLine();
-                    out = new PrintWriter(clientSocket.getOutputStream(), true);
-                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    out.println(name);
-                    resp = in.readLine();
-
-
-                    if (resp.equals("q")) {
-                        out.close();
-                        in.close();
-                        clientSocket.close();
-                    }else {
-                        toString();
-                    }
-
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
+        try {
+            user = new BufferedReader(new InputStreamReader(System.in));                    // Ник клиентов
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));   // Переменая принемает сообщения
+            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));  // Переменая отпровляет сообщения
+            userNick = this.nickUser();
+            new ReadChat().start();
+            new WriteChat().start();
+        } catch (IOException e) {
+            endSocket();
+        }
     }
 
-    public String toString() {
-        System.out.println(resp);
-        return resp;
+    public static void main(String[] args) throws IOException {
+        new Client();
+
+    }
+
+    public String nickUser() {                      // Сохроняет и отпровляет клиента на сервер
+        System.out.println("Ведите вааш ник !");
+        try {
+            userNick = user.readLine();
+            out.write("Добро пожаловать " + userNick + " ! " + "\n");
+            out.flush();
+        } catch (IOException ignored) {
+
+        }
+        return userNick;
+    }
+
+    private void endSocket() {                  // Отключает клента
+        try {
+            if (!clientSocket.isClosed()) {
+                clientSocket.close();
+                in.close();
+                out.close();
+            }
+        } catch (IOException ignored) {
+        }
+        clientSocket.isClosed();
+    }
+
+    public class ReadChat extends Thread {  // Клиент принемает сообщения
+        @Override
+        public void run() {
+            String msgRead;
+            try {
+                while (true) {
+                    msgRead = in.readLine();
+                    if (msgRead.equals("/exit")) {
+                        endSocket();
+                        break;
+                    }
+                    System.out.println(msgRead);
+                    System.out.print("->");
+                }
+            } catch (IOException e) {
+                endSocket();
+            }
+        }
+    }
+
+    public class WriteChat extends Thread {  // Клиент отпровляет сообщения
+        @Override
+        public void run() {
+            while (true) {
+                String msgWrite;
+                try {
+                    msgWrite = user.readLine();
+                    if (msgWrite.equals("/exit")) {
+                        out.write("/exit" + "\n");
+                        endSocket();
+                        break;
+                    } else {
+                        out.write(new SimpleDateFormat("dd.MM.yyyy HH.mm.ss ").format(Calendar.getInstance().getTime())
+                        + userNick + " : " + msgWrite + "\n");
+                    }
+                    out.flush();
+                } catch (Exception e) {
+                    endSocket();
+                }
+
+            }
+        }
     }
 }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -3,60 +3,96 @@ package org.example;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-public class Server {
-    protected static List<String> users = new ArrayList<>();
-    protected static ServerSocket server;
-    protected static PrintWriter out;
-    protected static BufferedReader in;
-    protected static String msg;
+public class Server extends Thread {
+    public static LinkedList<Server> serverList = new LinkedList<>();
+    protected final PrintWriter out;
+    public final BufferedReader in;
+    private final Socket socket;
 
-    public static void main(String[] args) throws IOException {
-        connect();
+    public Server(Socket socket) throws IOException {
+        this.socket = socket;
+        out = new PrintWriter(socket.getOutputStream(), true);  // переменая отпровляет сообщения
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // переменая принемает сообщения
+        start();
     }
 
-    public static void connect() throws IOException {
+    public static void main(String[] args) throws IOException {
+
         System.out.println("Запуск сервера");
-        String url = "/home/acer/IdeaProjects/ServerChat/settings.txt";
+        String url = "/home/acer/IdeaProjects/ServerChat/settings.txt";    // Запуск сервера через файл
         File settings = new File(url);
         Scanner scanner1 = new Scanner(settings);
         String hostPort = scanner1.nextLine();
         String[] hostPortMas = hostPort.split(":");
         int port = Integer.parseInt(hostPortMas[1]);
-        server = new ServerSocket(port);
-        Socket client = server.accept();
-        new Thread(() -> {
 
-            try {
-                while (true) {
-                    System.out.println(client);
-                    out = new PrintWriter(client.getOutputStream(), true);
-                    in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        try (ServerSocket server = new ServerSocket(port)) {
 
-                    msg = in.readLine();
-                    //new MsgClient(client);
 
-                    setAdd(msg);
-                    out.println(users);
+            while (true) {
+                Socket socket = server.accept();
+                try {
+                    serverList.add(new Server(socket));
+                    System.out.println("Подключился клиент : " + socket);
+                } catch (IOException e) {
+                    socket.close();
                 }
-
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
-
-        }).start();
+        }
     }
 
-    public static void setAdd(String msg) {
-        users.add(msg);
+    @Override
+    public void run() {  // поток приходят сообщения от клиента
+        String msg;
+        try {
+            msg = in.readLine();
+            out.write(msg + "\n");
+            out.flush();
+            while (true) {
+                try {
+                    msg = in.readLine();
+                    if (msg.equals("/exit")) {
+                        this.endSocket();
+                        break;
+                    }
+                } catch (NullPointerException ignored) {
+
+                }
+                System.out.println(msg);
+                for (Server msgs : serverList) {  // переберает и отпровляет сообщения клиенту
+                    msgs.msgSend(msg);
+                }
+            }
+        } catch (IOException e) {
+            this.endSocket();
+        }
     }
 
-    public void end() throws IOException {
-        server.close();
+    private void msgSend(String msg) { // отпровляет сообщения клиенту
+        try {
+            out.write(msg + "\n");
+            out.flush();
+        } catch (Exception ignored) {
+        }
+    }
+
+    protected void endSocket() { // отключате клиента
+        try {
+            if (!socket.isClosed()) {
+                socket.close();
+                in.close();
+                out.close();
+                for (Server vr : serverList) {
+                    if (vr.equals(this)) {
+                        vr.interrupt();
+                    }
+                    serverList.remove(this);
+                }
+                System.out.println("Клиент выключился!");
+            }
+        } catch (IOException ignored) {
+        }
     }
 }
-
