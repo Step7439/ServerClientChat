3e7fb0e8b17caa48d6cd7aadea7a65ad02f9b461
Step7439 <zamashikovs@mail.ru>
2022-09-19 20:04:11 +0900

    Add Test

---
+++ src/test/java/org/example/ServerTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerTest {
+
+        Server server = Mockito.mock(Server.class);
+
+    @Test
+    void run() {
+        assertNull(server.in);
+        assertNull(server.out);
+    }
+
+    @Test
+    void endSocket() {
+    }
+
+    @Test
+    void logServer() {
+    }
+}
